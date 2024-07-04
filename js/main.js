54e060e6720ff9cbd98b68fc3087be1e544e6680
kingmandoo <mjnwang@naver.com>
2024-07-04 18:11:30 +0900

    edit07040611

--- js/main.js
+++ js/main.js
@@ -10,33 +10,67 @@ window.onload=function(){
     let about_txtframe = document.querySelector(".about .txtframe")
     let redesign_imgframe = document.querySelectorAll(".redesign_1 .imgframe")
     let redesign_txtframe = document.querySelectorAll(".redesign_1 .txtframe")
+    let paths = document.querySelectorAll("path.naming")
+    let redesign_1 = document.querySelector(".redesign_1")
+    let redesign_2 = document.querySelector(".redesign_2")
+    let end = document.querySelector(".end")
+    let contact = document.querySelector(".contact")
+console.log(redesign_1)
+  let imgs = document.querySelectorAll(".redesign_1 .frame .imgframe>h1>img")
+
+
+
+// Playwrite AU NSW
+
+
+    for(let i=0;i<paths.length;i++){
+      paths[i].style.strokeDashoffset = paths[i].getTotalLength()
+      paths[i].style.strokeDasharray = paths[i].getTotalLength()
+      paths[i].style.animation = `naming_ani ${1 + i*0.1}s linear 2s forwards`
+    }
 
     function wheelDown(article, baseElementTop, clientHeight) {
         for (let i = 1; i < article.length; i++) {
             
             if(baseElementTop === clientHeight*5){
-                console.log("ddd")
                 cloud[0].style.transform = `translateX(-100%)`
                 cloud[1].style.transform = `translateX(100%)`
-                introduce.classList.add("open")
+                setTimeout(function(){
+                  introduce.classList.add("open")
+                },700)
+                
                 setTimeout(function(){
                     scrollTo({
                         top: clientHeight * 1,
                         behavior : "instant"
                     })
                     about.classList.add("close")
-                    about_imgframe.classList.add("downtoup")
-                    about_txtframe.classList.add("downtoup")
-                },1700)
+                  
+                },1600)
+                setTimeout(function(){
+                  about_imgframe.classList.add("downtoup")
+                  about_txtframe.classList.add("downtoup")
+                },2100)
             }
 
             if(baseElementTop === clientHeight*4){
-                        scrollTo({
-                            top: clientHeight * 2,
-                            behavior : "smooth"
-                        })
+              // about.classList.remove("close")
+              setTimeout(function(){
+                // about.classList.add("open")
+              },10)
+                    
+              scrollTo({
+                top: clientHeight * 2,
+                behavior : "smooth"
+            })
+                        setTimeout(function(){
+                          
                         redesign_imgframe[0].classList.add("downtoup")
                         redesign_txtframe[0].classList.add("downtoup")
+                        // about.classList.remove("open")
+                        // redesign_1.classList.add("close")
+                        },500)
+                        
                     }
 
             if(baseElementTop === clientHeight*3){
@@ -44,22 +78,46 @@ window.onload=function(){
                     top: clientHeight * 3,
                     behavior : "smooth"
                 })
-                redesign_imgframe[1].classList.add("downtoup")
+                // setTimeout(function(){
+                //   redesign_imgframe[0].classList.remove("downtoup")
+                //   redesign_txtframe[0].classList.remove("downtoup")
+                // },1000)
+                setTimeout(function(){
+
+                  redesign_imgframe[1].classList.add("downtoup")
                 redesign_txtframe[1].classList.add("downtoup")
+                },500)
+                
+
             }
 
             if(baseElementTop === clientHeight*2){
-                scrollTo({
-                    top: clientHeight * 4,
-                    behavior : "smooth"
-                })
+              // redesign_2.classList.remove("close")
+
+              scrollTo({
+                top: clientHeight * 4,
+                behavior : "smooth"
+            })
+              setTimeout(function(){
+                
+                // redesign_2.classList.add("open")
+              },100)
+              
+              setTimeout(function(){
+                // redesign_2.classList.remove("open")
+                // contact.classList.add("close")
+              
+              },900)
+                
             }
 
             if(baseElementTop === clientHeight*1){
+              
                 scrollTo({
-                    top: clientHeight *5,
-                    behavior : "smooth"
-                })
+                  top: clientHeight *5,
+                  behavior : "instant"
+              })
+                
             }
             
               }
@@ -78,13 +136,9 @@ window.onload=function(){
 
 
       function wheelUp(article, baseElementTop, clientHeight) {
+
         for (let i = 0; i < article.length - 1; i++) {
-          if (baseElementTop === clientHeight * i) {
-            scrollTo({
-              top: clientHeight * (article.length - 2 - i),
-              behavior: "smooth",
-            });
-          }
+          
           if(baseElementTop === clientHeight*4){
             introduce.style.opacity = `1`
             bg_gradient.style.opacity = `0`
@@ -92,18 +146,89 @@ window.onload=function(){
             cloud[1].style.transform = `translateX(0)`
             introduce.classList.remove("open")
             about.classList.remove("close")
-            setTimeout(function(){
-                about_imgframe.classList.remove("downtoup")
-                about_txtframe.classList.remove("downtoup")
-            },1000
-        )
-            
-
-
+            scrollTo({
+              top: clientHeight *0,
+              behavior : "smooth"
+          })
+           
+        
           }
+
+          if(baseElementTop === clientHeight*3){
+            console.log("하하하하")
+            // redesign_1.classList.remove("close")
+            // about.classList.remove("open")
+            scrollTo({
+              top: clientHeight *1,
+              behavior : "smooth"
+          })
+              
+                        setTimeout(function(){
+                          about_imgframe.classList.add("downtoup")
+                          about_txtframe.classList.add("downtoup")
+                        
+                        // redesign_1.classList.remove("open")
+                        // about.classList.add("close")
+                        // redesign_imgframe[0].classList.remove("downtoup")
+                        // redesign_txtframe[0].classList.remove("downtoup")
+                        },500)
+                        
+        }
+        
+
+        if(baseElementTop === clientHeight*2){
+          scrollTo({
+              top: clientHeight * 2,
+              behavior : "smooth"
+          })
+
+          // setTimeout(function(){
+          //   redesign_imgframe[0].classList.add("downtoup")
+          //   redesign_txtframe[0].classList.add("downtoup")
+          // },600)
+          // setTimeout(function(){
+          //   redesign_imgframe[1].classList.remove("downtoup")
+          //   redesign_txtframe[1].classList.remove("downtoup")
+          // },900)
+          
+      }
+
+
+
+
+      if(baseElementTop === clientHeight*1){
+          // contact.classList.remove("close")
+        // setTimeout(function(){
+        //   contact.classList.add("open")
+        // },10)
+
+        scrollTo({
+          top: clientHeight *3,
+          behavior : "smooth"
+      })
+        // setTimeout(function(){
+        // contact.classList.remove("open")
+        // redesign_2.classList.add("close")
+        // },900)
+         
+        // setTimeout(function(){
+        //   redesign_imgframe[1].classList.add("downtoup")
+        //   redesign_txtframe[1].classList.add("downtoup")
+        // },1000)
+      }
         }
       }
 
+
+      // function wheelUp(article, baseElementTop, clientHeight) {
+      //   for (let i = 0; i < article.length - 1; i++) {
+      //     if (baseElementTop === clientHeight * i) {
+      //       scrollTo({
+      //         top: clientHeight * (4 - i),
+      //         behavior: "smooth",
+      //       });
+      //     }
+      //   }
 
     
       document.addEventListener("wheel", (event) => {
